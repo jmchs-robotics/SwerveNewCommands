e43a1b5c4ace3bdaa8930e195314580b82efddc9
Eric Hall <ewhall18@gmail.com>
2020-01-06 11:44:55 -0700

    Testing GameSpecificMessage()

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,7 +7,9 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -96,6 +98,8 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void teleopPeriodic() {
+    // Pull down the match specific string and put it on the Dashboard
+    SmartDashboard.putString("Match String", DriverStation.getInstance().getGameSpecificMessage());
   }
 
   @Override
