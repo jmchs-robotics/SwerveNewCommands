9a740d20c31edf225b59389c8bbb72ee27e05ac7
Jon W. Hall, Jr <jhjh87@gmail.com>
2020-01-18 13:01:45 -0700

    tweak thrower

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -108,7 +108,7 @@ public final class Constants {
     public static final double kP = 0.001;
     public static final double kI = 0;
     public static final double kD = 0;
-    public static final double FEED_FORWARD = 0.01; // minimum useful 1/18 seems to be 0.001
+    public static final double FEED_FORWARD = 0.001; // minimum useful 1/18 seems to be 0.001
     public static final double kIz = 0;
     public static final double MIN_OUTPUT = -1;
     public static final double MAX_OUTPUT = 1;
