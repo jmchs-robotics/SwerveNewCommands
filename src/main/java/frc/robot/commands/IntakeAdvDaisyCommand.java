78def535ce1e1f96f80868ccc523ab74060f2468
Eric Hall <ewhall18@gmail.com>
2020-02-27 10:12:38 -0500

    Fix IntakeAdvDaisyCommand... Now extends ConditionalCommand properly.

--- src/main/java/frc/robot/commands/IntakeAdvDaisyCommand.java
+++ src/main/java/frc/robot/commands/IntakeAdvDaisyCommand.java
@@ -13,12 +13,7 @@ import edu.wpi.first.wpilibj2.command.*;
 import frc.robot.subsystems.HopperSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 
-public class IntakeAdvDaisyCommand extends CommandBase {
-  //private final ColorMatch m_colorMatcher = new ColorMatch();
-
-  private IntakeSubsystem m_Intake;
-  private HopperSubsystem m_Hopper;
- 
+public class IntakeAdvDaisyCommand extends ConditionalCommand {
 
   /**
    * Run the intake beater bar 'forward,' for collecting game pices (balls).
@@ -26,53 +21,31 @@ public class IntakeAdvDaisyCommand extends CommandBase {
   public IntakeAdvDaisyCommand(IntakeSubsystem intake, HopperSubsystem hopper) {
     // Use addRequirements() here to declare subsystem dependencies.
     // Requires the ControlPanel Subsystem
-    addRequirements(intake, hopper);
 
-    m_Intake = intake;
-    m_Hopper = hopper;
-    
-    new ConditionalCommand(
+    // This is just the constructor for a conditional command, wrapped in the IntakeAdvDaisyCommand name
+    // super(Command onTrue, Command onFalse, BooleanSupplier condition)
+    super(
       new SequentialCommandGroup(
-          //new InstantCommand(m_Intake :: lowerIntake, m_Intake),
+          //new InstantCommand(intake :: lowerIntake, intake),
           new ParallelRaceGroup(
-            new IntakeRecieveCommand(m_Intake),
-            new WaitUntilCommand(()->{ return m_Hopper.ballLoaded();}) 
+            new IntakeRecieveCommand(intake),
+            new WaitUntilCommand(()->{ return hopper.ballLoaded();}) 
           ),
-          new InstantCommand( m_Hopper::incBallCount, m_Hopper),
+          new InstantCommand( hopper::incBallCount, hopper),
           new ConditionalCommand( // if not full, advance Daisy; if full, reverse intake
               new SequentialCommandGroup(
-                new IntakeReversePulseCommand( m_Intake),
-                new BumpHopperCommand(m_Hopper),
-                new MoveHopperCommand(m_Hopper, 1),
-                new InstantCommand( m_Hopper::incBallCount, m_Hopper)
+                new IntakeReversePulseCommand( intake),
+                new BumpHopperCommand(hopper),
+                new MoveHopperCommand(hopper, 1),
+                new InstantCommand( hopper::incBallCount, hopper)
               ),
-              new IntakeReverseCommand( m_Intake),
-              () -> {return m_Hopper.daisyIsFull();} 
+              new IntakeReverseCommand( intake),
+              () -> {return hopper.daisyIsFull();} 
           )
         ),
-      new IntakeReverseCommand( m_Intake),
-      () -> {return m_Hopper.daisyIsFull();} // The conditional: if there are less than 5 balls, intake; else, backdrive
+      new IntakeReverseCommand( intake),
+      () -> {return hopper.daisyIsFull();} // The conditional: if there are less than 5 balls, intake; else, backdrive
     );
   }
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-  }
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-  }
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-  }
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
 }
